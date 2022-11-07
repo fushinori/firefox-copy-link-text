b96f249e1a44a0ccd1c6bb305ffce4e0df3fc695
Rishi <git@fushinori.me>
2022-11-07 16:24:28 +0530

    Initial commit

---
+++ copy_link_text.js
@@ -0,0 +1,11 @@
+const links = document.getElementsByTagName("a");
+
+for (link of links) {
+  link.addEventListener("dblclick", onDoubleClick);
+}
+
+function onDoubleClick(e) {
+  const link = e.currentTarget;
+  const linkText = link.textContent;
+  navigator.clipboard.writeText(linkText);
+}
---
+++ icons/ATTRIBUTION.md
@@ -0,0 +1,1 @@
+This [icon](https://icons8.com/icon/56307/copy-to-clipboard) is provided by [Icons8](https://icons8.com).
